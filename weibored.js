03edb0ad5cb5e3ce15c76feacf25d0987543cf86
Ruipeng Zhang <ppoffice@users.noreply.github.com>
2016-06-22 16:00:31 +0800

    A little improvement
    * setTimeout改成了setInterval
    * 减少了每次删除的等待时间

--- weibored.js
+++ weibored.js
@@ -12,12 +12,9 @@
 var s = document.createElement("script");
 s.setAttribute("src","https://lib.sinaapp.com/js/jquery/2.0.3/jquery-2.0.3.min.js");
 s.onload = function(){
-    for(var i=0;i<100;i++){
-        setTimeout(function(){
-            $('a[action-type="fl_menu"]')[0].click();
-            $('a[title="删除此条微博"]')[0].click();
-            $('a[action-type="ok"]')[0].click();
-        },1000*i);
-    }
+    setInterval(function(){
+        $('a[title="删除此条微博"]')[0].click();
+        $('a[action-type="ok"]')[0].click();
+    }, 800);
 }
 document.head.appendChild(s);
