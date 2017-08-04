0ce1a31819b4ab8e914ba77e4b64c859f4eb293d
artiship <meilziner@gmail.com>
2017-08-04 16:46:07 +0800

    Update weibored.js
    1. 自动翻页
    2. 触发自动加载

--- weibored.js
+++ weibored.js
@@ -9,12 +9,21 @@
 // ==/UserScript==
 'use strict';
 
+
 var s = document.createElement("script");
 s.setAttribute("src","https://lib.sinaapp.com/js/jquery/2.0.3/jquery-2.0.3.min.js");
 s.onload = function(){
     setInterval(function(){
-        $('a[title="删除此条微博"]')[0].click();
-        $('a[action-type="ok"]')[0].click();
+    	if(!$('a[action-type="feed_list_delete"]')) {
+    		$('a.next').click();
+    	} else {
+    		$('a[action-type="feed_list_delete"]')[0].click();
+        	$('a[action-type="ok"]')[0].click();
+    	}
+
+    	// scroll bottom let auto load
+        $("html, body").animate({ scrollTop: $(document).height() }, "slow");
     }, 800);
 }
 document.head.appendChild(s);
+
