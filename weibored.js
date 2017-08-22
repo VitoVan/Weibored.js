a70aef179495ee403bceead4d1298f174df7148e
Vito Van <awesomevito@live.com>
2017-08-22 17:25:40 +0800

    chore: :bookmark: versio bump

--- weibored.js
+++ weibored.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Weibored.js
 // @namespace    http://vito.sdf.org
-// @version      0.1
+// @version      0.2.0
 // @description  删除所有微博
 // @author       Vito Van
 // @match        http://weibo.com/p/*
@@ -9,21 +9,22 @@
 // ==/UserScript==
 'use strict';
 
+var s = document.createElement('script');
+s.setAttribute(
+  'src',
+  'https://lib.sinaapp.com/js/jquery/2.0.3/jquery-2.0.3.min.js'
+);
+s.onload = function() {
+  setInterval(function() {
+    if (!$('a[action-type="feed_list_delete"]')) {
+      $('a.next').click();
+    } else {
+      $('a[action-type="feed_list_delete"]')[0].click();
+      $('a[action-type="ok"]')[0].click();
+    }
 
-var s = document.createElement("script");
-s.setAttribute("src","https://lib.sinaapp.com/js/jquery/2.0.3/jquery-2.0.3.min.js");
-s.onload = function(){
-    setInterval(function(){
-    	if(!$('a[action-type="feed_list_delete"]')) {
-    		$('a.next').click();
-    	} else {
-    		$('a[action-type="feed_list_delete"]')[0].click();
-        	$('a[action-type="ok"]')[0].click();
-    	}
-
-    	// scroll bottom let auto load
-        $("html, body").animate({ scrollTop: $(document).height() }, "slow");
-    }, 800);
-}
+    // scroll bottom let auto load
+    $('html, body').animate({ scrollTop: $(document).height() }, 'slow');
+  }, 800);
+};
 document.head.appendChild(s);
-
